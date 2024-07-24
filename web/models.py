cf7b5eec2e98b69e12a5eff2d37f412e861db4b2
adithya4080 <adithyas712@gmail.com>
2024-07-24 17:40:21 +0530

    update

--- web/models.py
+++ web/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    icon = models.FileField(upload_to='category/icons/', blank=True)
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "web_category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ratings = models.IntegerField()
+    duration = models.IntegerField()
+    created_by = models.CharField(max_length=25)
+    languages = models.TextField()
+    thumbnail_img = models.ImageField(upload_to='courses/thumbnail', blank=True)
+    original_price = models.IntegerField()
+    discount_price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "web_course"
+        verbose_name_plural = "courses"
+
+    def __str__(self):
+        return self.title
