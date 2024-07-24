277ede18855f3fd2aa8dbf6cc17d852dff20da07
adithya4080 <adithyas712@gmail.com>
2024-07-24 10:56:18 +0530

    updates

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.cust_bot');
+    const prev = document.getElementById('prev');
+    const next = document.getElementById('next');
+
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        const slideWidth = slides[0].clientWidth + 40; // Adjust according to the padding/margin
+        slider.style.transform = `translateX(-${index * slideWidth}px)`;
+    }
+
+    prev.addEventListener('click', function() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showSlide(currentIndex);
+        }
+    });
+
+    next.addEventListener('click', function() {
+        if (currentIndex < slides.length - 3) {
+            currentIndex++;
+            showSlide(currentIndex);
+        }
+    });
+});
--- web/views.py
+++ web/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def singlepage(request):
+    return render(request, "singlepage.html")
