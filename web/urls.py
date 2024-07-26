b3b9ae19b867a5b3f8806283d64b27a842a29bac
adithya4080 <adithyas712@gmail.com>
2024-07-26 15:26:31 +0530

    updates

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from web.views import index, singlepage, courses
+from . import views
 
 
 app_name = 'web'
@@ -7,5 +8,6 @@ app_name = 'web'
 urlpatterns = [
     path('',index,name="index"),
     path('singlepage/<int:course_id>/',singlepage,name="singlepage"),
-    path('courses/',courses,name="courses")
+    path('courses/',courses,name="courses"),
+    path('category/<int:category_id>/',views.category_courses,name="category_courses")
 ]
--- web/views.py
+++ web/views.py
@@ -24,19 +24,41 @@ def index(request):
     return render(request, "index.html", context=context)
 
     
+def category_courses(request, category_id):
+    print(f"Received category_id: {category_id}") 
+    category = get_object_or_404(Category, id=category_id)
+    courses = Course.objects.filter(category=category)
+
+    for course in courses:
+        original_price = course.original_price
+        discount_percentage = course.discount_price
+        course.new_price = original_price - (original_price * (discount_percentage / 100))
+
+    context = {
+        "category": category,
+        "courses": courses
+    }
+
+    return render(request, "categorys.html", context=context)
 
 
 def singlepage(request,course_id):
-    courses = Course.objects.all()[:4]
     course = get_object_or_404(Course, id=course_id)
     original_price = course.original_price
     discount_percentage = course.discount_price
     new_price = original_price - (original_price * (discount_percentage / 100))
+    category = course.category
+    similar_courses = Course.objects.filter(category=category).exclude(id=course_id).distinct()[:4]
+
+    for similar_course in similar_courses:
+        original_price = similar_course.original_price
+        discount_percentage = similar_course.discount_price
+        similar_course.new_price = original_price - (original_price * (discount_percentage / 100))
 
     context = {
         "courses" : course,
         'new_price': new_price,
-        "courses" : courses
+        'similar_courses': similar_courses
     }
 
     return render(request, "singlepage.html",context=context)
@@ -55,3 +77,6 @@ def courses(request):
     }
 
     return render(request,"courses.html", context=context)
+
+def categorys(request):
+    return render(request, "categorys.html")
